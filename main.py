13e8eb3ffee033880246d45d1b0abf4f4124d92f
Scott Essner <scott.essner@gmail.com>
2019-07-05 19:21:44 -0500

    Updated to persist the container across multiple tasks.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from task.client import TaskClient
 import os
+import time
 
 print(os.environ['API_URL'])
 
 client = TaskClient(os.environ['API_URL'])
 
-# while True:
-task = client.take()
+while True:
+    task = client.take()
 
-if task is not None:
-    try:
-        task.execute()
-    except Exception:
-        task.error()
+    if task is not None:
+        try:
+            task.execute()
+        except Exception:
+            task.error()
 
+    else:
+        print("No new tasks.  Sleeping for 60 seconds")
+        time.sleep(60)
